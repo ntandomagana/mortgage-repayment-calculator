b2590f5d6dba87ba56bc511fc916499204fa9c68
ntandomagana <ntandomagana@gmail.com>
2024-10-11 11:02:22 +0200

    Fixed JS calculation button

--- main.js
+++ main.js
@@ -1,125 +1,128 @@
-const defaultText = document.getElementById('default-text')
-const calculationsContainer = document.getElementById('calculations-container')
+const defaultText = document.getElementById('default-text');
+const calculationsContainer = document.getElementById('calculations-container');
 
 //form handling where the user can select diff types of mortgages an duser can choose + select one option
 document.querySelectorAll('.mortgage-type').forEach(input => { // looks for this 'mortgae-type' element and stores it in this variable
     input.addEventListener('change', function() {
         document.querySelectorAll('.radio-inputs').forEach(div => {
             div.classList.remove('selected');
-        })
+        });
 
         if (this.checked) {
-            this.parentElement.classList.add('selected')
+            this.parentElement.classList.add('selected');
         }
-    })
+    });
 
-})
+});
  // when the user clicks this button, the function will be executed
 document.getElementById('calculate-btn').addEventListener('click', () => {
 
     //this retrieves the value of the ID's and changes it to a floating-point number.
-    const amount = parseFloat(document.getElementById('mortgage-amount').value)
-    const term = parseFloat(document.getElementById('mortgage-term').value)
+    const amount = parseFloat(document.getElementById('mortgage-amount').value);
+    const term = parseFloat(document.getElementById('mortgage-term').value);
     const rate = parseFloat(document.getElementById('interest-rate').value) / 100
-    const mortgageType = document.querySelector('input[name="mortgage-type"]:checked')
+    const mortgageType = document.querySelector('input[name="mortgage-type"]:checked');
 
     //validates the inputs
     let isValid = true;
 
 
 // removes error from elements with this class
-    document.querySelectorAll('.form-flex')forEach(el => {
-        el.classList.remove('error')
+    document.querySelectorAll('.form-flex').forEach(el => {
+        el.classList.remove('error');
     })
 
     //this function checks if user has entered a numeric value, not negative or below 0
     if(isNaN(amount) || amount <= 0) { 
         //this happens if amount is invalid
         document.getElementById('amount-alert').style.display = 'block'
-        document.getElementById('mortgage-amount-main').classList.add('error')
-        isValid = false
+        document.getElementById('mortgage-amount-main').classList.add('error');
+        isValid = false;
 
         // this else statement will remove error messages if the input is valid
     } else {
-        document.getElementById('amount-alert').style.display = 'none'
+        document.getElementById('amount-alert').style.display = 'none';
     }
 // This code is similar to previous oone, but checks the term. If term number is valid
 // then error messages are hidden
     if(isNaN(term) || term <= 0) {
-        document.getElementById('term-alert').style.display = 'block'
-        document.getElementById('mortgage-term-main').classList.add('error')
-        isValid = false
+        document.getElementById('term-alert').style.display = 'block';
+        document.getElementById('mortgage-term-main').classList.add('error');
+        isValid = false;
     } else {
-        document.getElementById('term-alert').style.display = 'none'
+        document.getElementById('term-alert').style.display = 'none';
     }
 
-    if(isNaN(rate) || rate <= 0) {
-        document.getElementById('rate-alert').style.display = 'block'
-        document.getElementById('interest-rate-main').classList.add('error')
-        isValid = false
-    } else {
-        document.getElementById('rate-alert').style.display = 'none'
-    }
+    // if(isNaN(rate) || rate <= 0) {
+    //     document.getElementById('rate-alert').style.display = 'block';
+    //     document.getElementById('interest-rate-main').classList.add('error');
+    //     isValid = false;
+    // } else {
+    //     document.getElementById('rate-alert').style.display = 'none';
+    // }
 
     if (!mortgageType) { // no mortgage type has been selected
-        document.getElementById('type-alert').style.display = 'block'
+        document.getElementById('type-alert').style.display = 'block';
         document.querySelectorAll('.radio-inputs').forEach(el => {
-            el.classList.add('error')
+            el.classList.add('error');
         })
 
-        isValid = false
+        isValid = false;
     } else {
-        document.getElementById('type-alert').style.display = 'none'
+        document.getElementById('type-alert').style.display = 'none';
         document.querySelectorAll('.radio-inputs').forEach(el => {
-            el.classList.remove('error')
+            el.classList.remove('error');
         })
     }
+    console.log("lee",isValid)
 
     if (isValid) {
         let monthlyPayment = 0;
         let totalRepayment = 0;
 
-        defaultText.classList.add('hide')
-        calculationsContainer.classList.add('hide')
+        defaultText.classList.add('hide');
+        calculationsContainer.classList.remove('hide');
 
         if(mortgageType.value === 'repayment') {
-            const monthlyRate = rate / 12
-            const n = term * 12
+            const monthlyRate = rate / 12;
+            const n = term * 12;
             monthlyPayment = (amount * monthlyRate) / (1 - Math.pow((1 + monthlyRate), - n))
-            totalRepayment = monthlyPayment * n
+            totalRepayment = monthlyPayment * n;
         } else if (mortgageType.value === 'interest-only') {
-            monthlyPayment = (amount * rate) / 12
-            totalRepayment = monthlyPayment * term * 12
+            monthlyPayment = (amount * rate) / 12;
+            totalRepayment = monthlyPayment * term * 12;
         }
 
-        document.getElementById('result').innerText = `$${monthlyPayment.toFixed(2)}`
-        document.getElementById('term-result').innerText = `$${totalRepayment.toFixed(2)}`
+        document.getElementById('result').innerText = `$${monthlyPayment.toFixed(2)}`;
+        document.getElementById('term-result').innerText = `$${totalRepayment.toFixed(2)}`;
     } else {
-        document.getElementById('result').innerText = ''
-        document.getElementById('term-result').innerText = ''
+        document.getElementById('result').innerText = '';
+        document.getElementById('term-result').innerText = '';
 
-        defaultText.classList.remove('hide')
-        calculationsContainer.classList.remove('show')
+        defaultText.classList.remove('hide');
+        calculationsContainer.classList.remove('show');
     }
 })
 
+
+//clears everything and resets the form
 document.getElementById('clear-btn').addEventListener('click', () => {
-    document.getElementById('mortgage-form').reset()
-    document.getElementById('result').innerText = ''
-    document.getElementById('term-result').innerText = ''
+    document.getElementById('mortgage-form').reset();
+    document.getElementById('result').innerText = '';
+    document.getElementById('term-result').innerText = '';
     document.querySelectorAll('.form-alert').forEach(alert => {
-        alert.style.display = 'none'
+        alert.style.display = 'none';
     })
 
-    defaultText.classList.remove('hide')
-    calculationsContainer.classList.remove('show')
+    defaultText.classList.remove('hide');
+    calculationsContainer.classList.remove('show');
 
     document.querySelectorAll('.form-flex').forEach(el => {
-        el.classList.remove('error')
+        el.classList.remove('error');
     })
 
 })
 
 document.querySelectorAll('.form-alert').forEach(alert => {
-    alert.style.display = 'none'
+    alert.style.display = 'none';
 })
